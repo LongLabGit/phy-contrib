b96b8c8e83a1d770b0fbd63e4b353ce619e9d2e6
vigi_katlowitz <akatlowitz@gmail.com>
2017-10-26 15:26:07 -0400

    took out gaussian fitting
    huge waste of time

--- phycontrib/views/amplitude_histogram.py
+++ phycontrib/views/amplitude_histogram.py
@@ -47,13 +47,13 @@ class AmplitudeHistogram(IPlugin):
         ax.get_yaxis().set_tick_params(direction='out')
         ax.get_xaxis().set_tick_params(direction='out')
 
-        def gaussian(x, a, x0, sigma):
-            return a * np.exp(-(x - x0) ** 2 / (2 * sigma ** 2))
+        #def gaussian(x, a, x0, sigma):
+        #    return a * np.exp(-(x - x0) ** 2 / (2 * sigma ** 2))
 
-        def gaussian_cut(x, a, x0, sigma, xcut):
-            g = a * np.exp(-(x - x0) ** 2 / (2 * sigma ** 2))
-            g[x < xcut] = 0
-            return g
+        #def gaussian_cut(x, a, x0, sigma, xcut):
+        #    g = a * np.exp(-(x - x0) ** 2 / (2 * sigma ** 2))
+        #    g[x < xcut] = 0
+        #    return g
 
         def _update(clusters):
             ax.clear()
@@ -71,34 +71,34 @@ class AmplitudeHistogram(IPlugin):
                                              orientation='horizontal')
 
                 #fit a gaussian to the histogram
-                mean_seed = coords.y.mean()
-                mean_seed = bins[np.argmax(num)]  # mode of mean_seed
-                bin_steps = np.diff(bins[:2])[0]
-                x = bins[:-1] + bin_steps / 2
-                next_low_bin = x[0] - bin_steps
-                add_points = np.flipud(np.arange(next_low_bin,
-                                                 0, -bin_steps))
-                x = np.append(add_points, x)
-                num = np.append(np.zeros(len(add_points)), num)
-                if 0:
-                    #old way, gaussian fit
-                    popt, pcov = curve_fit(gaussian,
-                                           x,
-                                           num,
-                                           p0=(num.max(),
-                                               mean_seed,
-                                               2 * coords.y.std()))
-                    n_fit = gaussian(x, popt[0], popt[1], popt[2])
-                    min_amplitude = coords.y.min()
-                    was_fit[i] = True
-                else:
-                    #new way, cutoff gaussian fit
-                    p0 = (num.max(), mean_seed, 2 * coords.y.std(),
-                          np.percentile(coords.y, 1))
-                    try:
-                        popt, pcov = curve_fit(gaussian_cut, x, num, p0=p0,
-                                               maxfev=10000)
-                        was_fit[i] = True
+        #        mean_seed = coords.y.mean()
+        #        mean_seed = bins[np.argmax(num)]  # mode of mean_seed
+        #        bin_steps = np.diff(bins[:2])[0]
+        #        x = bins[:-1] + bin_steps / 2
+        #        next_low_bin = x[0] - bin_steps
+        #        add_points = np.flipud(np.arange(next_low_bin,
+        #                                         0, -bin_steps))
+        #        x = np.append(add_points, x)
+        #        num = np.append(np.zeros(len(add_points)), num)
+        #        if 0:
+        #            #old way, gaussian fit
+        #            popt, pcov = curve_fit(gaussian,
+        #                                   x,
+        #                                   num,
+        #                                   p0=(num.max(),
+        #                                       mean_seed,
+        #                                       2 * coords.y.std()))
+        #            n_fit = gaussian(x, popt[0], popt[1], popt[2])
+        #            min_amplitude = coords.y.min()
+        #            was_fit[i] = True
+        #        else:
+        #            #new way, cutoff gaussian fit
+        #            p0 = (num.max(), mean_seed, 2 * coords.y.std(),
+        #                  np.percentile(coords.y, 1))
+        #            try:
+        #                popt, pcov = curve_fit(gaussian_cut, x, num, p0=p0,
+        #                                       maxfev=10000)
+        #                was_fit[i] = True
                     #except Exception as e:
                      #   try:
                      #       logger.info("Fitting failed with maxfev=10000"
@@ -106,29 +106,29 @@ class AmplitudeHistogram(IPlugin):
                      #       popt, pcov = curve_fit(gaussian_cut, x, num,
                      #                              p0=p0, maxfev=1000000)
                      #       was_fit[i] = True
-                    except Exception as e:
-                        logger.info("Fitting error: %s", str(e))
-                        was_fit[i] = False
-                    if was_fit[i]:
-                        n_fit = gaussian_cut(x, popt[0], popt[1],
-                                             popt[2], popt[3])
-                        min_amplitude = popt[3]
-                        n_fit_no_cut = gaussian_cut(x, popt[0], popt[1],
-                                                    popt[2], 0)
-                        ax.plot(n_fit_no_cut, x, c='w', linestyle='--')
-                if was_fit[i]:
-                    maxs[i] = n_fit.max()
-                    ax.plot(n_fit, x, c='w')
-                    # norm area calculated by fit parameters
-                    norm_area_ndtr = ndtr((popt[1] - min_amplitude) /
-                                          popt[2])
-                    percent_missing_ndtr = 100 * (1 - norm_area_ndtr)
+        #            except Exception as e:
+        #                logger.info("Fitting error: %s", str(e))
+        #                was_fit[i] = False
+        #            if was_fit[i]:
+        #                n_fit = gaussian_cut(x, popt[0], popt[1],
+        #                                     popt[2], popt[3])
+        #                min_amplitude = popt[3]
+        #                n_fit_no_cut = gaussian_cut(x, popt[0], popt[1],
+        #                                            popt[2], 0)
+        #                ax.plot(n_fit_no_cut, x, c='w', linestyle='--')
+        #        if was_fit[i]:
+        #            maxs[i] = n_fit.max()
+        #            ax.plot(n_fit, x, c='w')
+        #            # norm area calculated by fit parameters
+        #            norm_area_ndtr = ndtr((popt[1] - min_amplitude) /
+        #                                  popt[2])
+        #            percent_missing_ndtr = 100 * (1 - norm_area_ndtr)
 
-                    logger.debug('Cluster %d is missing %.1f of spikes',
-                                 clusters[i], percent_missing_ndtr)
+        #            logger.debug('Cluster %d is missing %.1f of spikes',
+        #                         clusters[i], percent_missing_ndtr)
 
-            if any(was_fit):
-                ax.set_xlim([0, maxs.max() * 1.3])
+        #    if any(was_fit):
+        #        ax.set_xlim([0, maxs.max() * 1.3])
             xt = ax.get_xticks()
             ax.set_xticks((xt[0], xt[-1]))
             f.canvas.draw()
