0e2ea6d635c0bada2bdea78cacf90e273d3d61b5
robert <robert.egger@nyumc.org>
2019-01-04 10:36:51 -0800

    Option to try estimating number of components for cluster cut

--- phycontrib/views/gmm_cluster_cut.py
+++ phycontrib/views/gmm_cluster_cut.py
@@ -27,6 +27,36 @@ class AutomatedClusterCut(IPlugin):
 
     def attach_to_controller(self, controller):
 
+        def _fit_gmm_fixed_n_components(pc, n_components):
+            logger.info('Fitting GMM...')
+            gmm = gm.GaussianMixture(n_components)
+            labels = gmm.fit_predict(pc)
+            logger.info('...done!')
+            return labels
+
+        def _fit_gmm_var_n_components(pc, max_components=None):
+            if max_components is None:
+                max_comp = 10
+            else:
+                max_comp = max_components
+
+            min_bic = np.inf
+            min_comp = 0
+            min_labels = []
+            logger.info('Fitting GMM and trying to estimate optimal number of components...')
+            for n_components in range(1, max_comp + 1):
+                gmm = gm.GaussianMixture(n_components)
+                gmm.fit(pc)
+                current_bic = gmm.bic(pc)
+                if current_bic < min_bic:
+                    min_bic = current_bic
+                    min_comp = n_components
+                    min_labels = gmm.predict(pc)
+
+            logger.info('...done!')
+            logger.info('Estimated number of components: %d' % min_comp)
+            return min_labels
+
         def _fit_gmm(cluster_ids, n_components):
             clustering = controller.supervisor.clustering
             for cluster in cluster_ids:
@@ -40,10 +70,10 @@ class AutomatedClusterCut(IPlugin):
             # pc.shape = n_spikes, n_channels_loc * n_pcs
             pc_ = controller.model.features[spike_ids]
             pc = np.reshape(pc_, (pc_.shape[0], pc_.shape[1] * pc_.shape[2]))
-            logger.info('Fitting GMM...')
-            gmm = gm.GaussianMixture(n_components)
-            labels = gmm.fit_predict(pc)
-            logger.info('...done!')
+            if n_components == 'var':
+                labels = _fit_gmm_var_n_components(pc)
+            else:
+                labels = _fit_gmm_fixed_n_components(pc, n_components)
             labels_ = np.unique(labels)
             for label in labels_[:-1]:
                 split_spike_ids = spike_ids[np.where(labels == label)]
@@ -54,11 +84,16 @@ class AutomatedClusterCut(IPlugin):
 
             @controller.supervisor.actions.add(alias='gmm')
             def automated_cluster_cut(n_components):
-                if n_components < 2:
-                    logger.error('GMM cluster cut requires at least 2 components in mixture')
-                else:
+                # TODO: add possibility to use PCs from specific channels
+                if n_components == 'var':
+                    logger.info('Starting GMM cluster cut with unknown number of components in mixture...')
+                    cluster_ids = controller.supervisor.selected
+                    _fit_gmm(cluster_ids, n_components)
+                elif n_components >= 2:
                     logger.info('Starting GMM cluster cut with %d components in mixture...' % n_components)
                     cluster_ids = controller.supervisor.selected
                     _fit_gmm(cluster_ids, n_components)
+                else:
+                    logger.error('GMM cluster cut requires at least 2 components in mixture; or -1 to estimate these')
 
 
